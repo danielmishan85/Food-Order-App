72aea59cf61e11ad71dd8fdad71d1bddaf76ca0d
danielmishan85 <danielmishan85@gmail.com>
2023-06-11 16:48:29 +0300

    start detail design ui

---
+++ app/src/main/java/com/example/foodorderapp/Activity/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.foodorderapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.foodorderapp.R;
+
+
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
