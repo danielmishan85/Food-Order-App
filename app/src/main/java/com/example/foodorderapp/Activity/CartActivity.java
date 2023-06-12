885e2b2e628d28fa3cd29b9bde108c78d0ff5e2a
danielmishan85 <danielmishan85@gmail.com>
2023-06-12 21:18:07 +0300

    finish design cart ui

---
+++ app/src/main/java/com/example/foodorderapp/Activity/CartActivity.java
@@ -0,0 +1,15 @@
+package com.example.foodorderapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.foodorderapp.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
